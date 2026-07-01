6c3dba84072d36ec44a23a3a0bbb8b63fd65d11e
Daniela Saavedra <lagos.dany96@gmail.com>
2026-07-01 08:47:32 -0400

    final game version

--- borrador2.py
+++
@@ -1,9 +0,0 @@
-seguir = "S"
-
-while seguir == "S" or seguir == "s":
-
-    jugador = input("\nIngresa tu nombre para iniciar la partida: ")
-
-    jugar(jugador)
-
-    seguir = input("\n¿Deseas jugar nuevamente? (S/N): ")
